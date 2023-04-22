8eaa292db662c5a00fd4c6c66be5b04ab07cc423
ARITSU <lynniswaifu@gmail.com>
2023-04-22 18:33:30 +0700

    Update Topping.jsx

--- src/components/Topping.jsx
+++ src/components/Topping.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   AppBar,
   Container,
@@ -6,34 +7,101 @@ import {
   Typography,
   Switch,
   Button,
+  IconButton,
+  Menu,
+  MenuItem,
 } from "@mui/material";
 import { Link } from "react-router-dom";
 
-import { BsSunFill, BsSun } from "react-icons/bs";
+import { BsSunFill, BsSun, AiOutlineMenu } from "react-icons/all";
+
 export default function Topping(props) {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <AppBar position="static">
       <Container>
-        <Toolbar style={{height:"23px"}}>
+        <Toolbar style={{ height: "23px" }}>
           <Link to="/" style={{ textDecoration: "none", color: "white" }}>
             <Typography variant="h3">ARITSU</Typography>
           </Link>
           <Box flexGrow={1} />
-          <Button component={Link} to="/" style={{ color: "white" }}>
-            Home
-          </Button>
-          <Button component={Link} to="/aboutme" style={{ color: "white" }}>
-            About me
-          </Button>
-          <Button style={{ color: "white" }}>
-            <a href="https://anithai.netlify.app/" sx={{ color: "white", textDecoration:"none" }}>AniThai</a>
-          </Button>
-          <Button
-            style={{ fontSize: "16px" , color: "white" }}
-            onClick={props.toggleTheme}
-          >
-            {props.themeNow ? <BsSun /> : <BsSunFill />}
-          </Button>
+          <Box sx={{ display: { xs: "block", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="menu"
+              onClick={handleClick}
+              sx={{ color: "white" }}
+            >
+              <AiOutlineMenu />
+            </IconButton>
+            <Menu
+              id="simple-menu"
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose} component={Link} to="/">
+                Home
+              </MenuItem>
+              <MenuItem onClick={handleClose} component={Link} to="/aboutme">
+                About me
+              </MenuItem>
+              <MenuItem onClick={handleClose}>
+                <a
+                  href="https://anithai.netlify.app/"
+                  style={{
+                    textDecoration: "none",
+                    color: props.themeNow ? "white" : "black",
+                  }}
+                >
+                  AniThai
+                </a>
+              </MenuItem>
+              <MenuItem onClick={handleClose} component={Link} to="/aboutme">
+                <Button
+                  style={{
+                    fontSize: "16px",
+                    color: props.themeNow ? "white" : "black",
+                  }}
+                  onClick={props.toggleTheme}
+                >
+                  {props.themeNow ? <BsSun /> : <BsSunFill />}
+                </Button>
+              </MenuItem>
+            </Menu>
+          </Box>
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            <Button component={Link} to="/" style={{ color: "white" }}>
+              Home
+            </Button>
+            <Button component={Link} to="/aboutme" style={{ color: "white" }}>
+              About me
+            </Button>
+            <Button style={{ color: "white" }}>
+              <a
+                href="https://anithai.netlify.app/"
+                style={{ color: "inherit", textDecoration: "none" }}
+              >
+                AniThai
+              </a>
+            </Button>
+            <Button
+              style={{ fontSize: "16px", color: "white" }}
+              onClick={props.toggleTheme}
+            >
+              {props.themeNow ? <BsSun /> : <BsSunFill />}
+            </Button>
+          </Box>
         </Toolbar>
       </Container>
     </AppBar>
