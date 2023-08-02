0f6f4d7f90f2851c5ec2737b7cbe419fabf7af99
Lynn <lynniswaifu@gmail.com>
2023-08-02 19:39:14 +0700

    theme+

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,6 @@ export default {
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
-    themes: ["light", "dark", "dracula"],
+    themes: ["light", "dark", "dracula", "black", "business"],
   },
 };
